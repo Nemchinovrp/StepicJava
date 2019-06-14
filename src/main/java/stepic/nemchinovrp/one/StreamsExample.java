7303e4d56fb044fef21fd7d559ace61424dfb934
Roman Nemchinov <nemchinovrp@yandex.ru>
2019-06-14 15:37:47 +0300

    added classes

--- src/main/java/stepic/nemchinovrp/one/StreamsExample.java
+++ src/main/java/stepic/nemchinovrp/one/StreamsExample.java
@@ -9,8 +9,20 @@ import static java.lang.Math.pow;
 
 public class StreamsExample {
     public static void main(String[] args) {
-        distinctStream();
+//        distinctStream();
+        filterExample();
+        limitExample(10);
+    }
 
+    private static void limitExample(int i) {
+        Random random = new Random();
+        random.ints().limit(i).forEach(System.out::println);
+    }
+
+    private static void filterExample() {
+        List<String> strings = Arrays.asList("abc", "", "bc", "efg", "abcd","", "jkl");
+        long count = strings.stream().filter(string -> string.length() >= 3).count();
+        System.out.println(count);
     }
 
     private static void distinctStream() {
@@ -23,6 +35,4 @@ public class StreamsExample {
         Random random = new Random();
         random.ints().limit(3).forEach(System.out::println);
     }
-
-
 }
